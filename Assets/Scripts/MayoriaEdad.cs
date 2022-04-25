55ac8a31ac8e53a414079ba76a49d862aff0700a
FranSifon <101577686+FranSifon@users.noreply.github.com>
2022-04-25 20:39:27 -0300

    Initial commit

---
+++ Assets/Scripts/MayoriaEdad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayoriaEdad : MonoBehaviour
+{
+
+    public int EdadUsuario;
+    public Text mitexto;
+    void Start()
+    {
+       if ( EdadUsuario >= 18)
+        {
+            mitexto.text = " Es mayor de edad ";
+            Debug.Log(" Es mayor de edad ");
+        }
+        else
+        {
+            mitexto.text = " Es menor de edad ";
+            Debug.Log(" No es mayor de edad ");
+        }
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ValidarContraseña.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ValidarContraseña : MonoBehaviour
+{
+    string contraseñacorrecta;
+    string contraseñausuario;
+    public Text ingresousuario;
+    public Text textomsj;
+    public GameObject cartel;
+    void Start()
+    {
+        contraseñacorrecta = "12345";
+        cartel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Validarcontraseña()
+    {
+        contraseñausuario = ingresousuario.text;
+        if (contraseñausuario == contraseñacorrecta)
+        {
+            cartel.SetActive(true);
+            textomsj.text = " Bienvenido ";
+            Debug.Log(" Bienvenido");
+        }
+        else
+        {
+            textomsj.text = " Contraseña incorrecta";
+            cartel.SetActive(true);
+            Debug.Log(" Contraseña incorrecta");
+        }
+    }
+}
